57a92238d6636a856f889b9bbf7bd4e675eda2ca
Mikhail <misha.rukovishnikow@yandex.ru>
2022-01-04 19:31:11 +0300

    feat: created basic solution structure

---
+++ Pig-n-Go.Server/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Pig_n_Go.Models {
+    public class ErrorViewModel {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
