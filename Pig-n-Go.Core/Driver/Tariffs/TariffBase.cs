3728327e21d9f8effe8d62dcd66e086febc12af7
Arctik <edgar.saratovcev@gmail.con>
2022-01-05 15:09:16 +0300

    fix: add Guid in TariffBase

--- Pig-n-Go.Core/Driver/Tariffs/TariffBase.cs
+++ Pig-n-Go.Core/Driver/Tariffs/TariffBase.cs
@@ -1,7 +1,10 @@
-﻿namespace Pig_n_Go.Driver.Tariffs
+﻿using System;
+
+namespace Pig_n_Go.Driver.Tariffs
 {
     public abstract class TariffBase
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
         public abstract decimal ChargePerLocationUnit { get; set; }
     }
 }
