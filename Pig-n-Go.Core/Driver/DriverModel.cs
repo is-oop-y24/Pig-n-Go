99ef1be0f8cc22d2c19b89fe78235287c2c7cf0e
Arctik <edgar.saratovcev@gmail.con>
2022-01-05 15:07:11 +0300

    feat: extend DriverModel

--- Pig-n-Go.Core/Driver/DriverModel.cs
+++ Pig-n-Go.Core/Driver/DriverModel.cs
@@ -1,7 +1,15 @@
-﻿namespace Pig_n_Go.Driver 
+﻿using System;
+using Pig_n_Go.Driver.Tariffs;
+
+namespace Pig_n_Go.Driver 
 {
-    public class DriverModel 
+    public class DriverModel
     {
-                
+        public Guid Id { get; set; }
+        public DriverInfo DriverInfo { get; set; }
+        public CarInfo CarInfo { get; set; }
+        public CartesianLocationUnit Location { get; set; }
+        public DriverRating DriverRating { get; set; }
+        public TariffBase Tariff { get; set; }
     }
 }
