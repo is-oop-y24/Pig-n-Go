a78f47fed180f17c3177b6dfb272becf00358c4d
Mikhail <misha.rukovishnikow@yandex.ru>
2022-01-05 14:52:38 +0300

    feat: added db context

---
+++ Pig-n-Go.Server/ServiceLogic/TaxiContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pig_n_Go.Driver;
+using Pig_n_Go.Order;
+using Pig_n_Go.Passenger;
+
+namespace Pig_n_Go
+{
+    public class TaxiContext : DbContext
+    {
+        public DbSet<DriverModel> DriversModels;
+        public DbSet<PassengerModel> PassengerModels;
+        public DbSet<OrderModel> OrderModels;
+
+        public TaxiContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
