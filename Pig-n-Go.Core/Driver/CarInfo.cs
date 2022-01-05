1a5ade640af6db0f9eb37bc53af514c6c8ad69ee
Arctik <edgar.saratovcev@gmail.con>
2022-01-05 15:23:22 +0300

    feat: add CarInfo

--- Pig-n-Go.Core/Driver/CarInfo.cs
+++ Pig-n-Go.Core/Driver/CarInfo.cs
@@ -1,7 +1,12 @@
-﻿namespace Pig_n_Go.Driver
+﻿using System;
+
+namespace Pig_n_Go.Driver
 {
     public class CarInfo
     {
-        
+        public Guid Id { get; init; }
+        public string ModelName { get; init; }
+        public string CarNumber { get; set; }
+        public string Color { get; set; }
     }
 }
