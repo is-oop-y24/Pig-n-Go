97c160a3a148fd0f7b633e7f01dc5ac523d0eba4
Arctik <edgar.saratovcev@gmail.con>
2022-01-05 15:07:59 +0300

    feat: extend DriverInfo

--- Pig-n-Go.Core/Driver/DriverInfo.cs
+++ Pig-n-Go.Core/Driver/DriverInfo.cs
@@ -1,7 +1,11 @@
-﻿namespace Pig_n_Go.Driver
+﻿using System;
+
+namespace Pig_n_Go.Driver
 {
     public class DriverInfo
     {
-        
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
     }
 }
