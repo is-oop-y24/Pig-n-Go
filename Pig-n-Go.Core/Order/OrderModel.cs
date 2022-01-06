88379dd435e5c59b23689505a25acda7395831f5
Alexander Blazhkov <thermo911@yandex.ru>
2022-01-06 10:53:43 +0300

    feat: extend OrderModel and PassengerModel

--- Pig-n-Go.Core/Order/OrderModel.cs
+++ Pig-n-Go.Core/Order/OrderModel.cs
@@ -1,7 +1,17 @@
-﻿namespace Pig_n_Go.Order
+﻿using System;
+using Pig_n_Go.Driver;
+using Pig_n_Go.Passenger;
+
+namespace Pig_n_Go.Order
 {
     public class OrderModel
     {
-        
+        public Guid Id { get; init; }
+        public OrderStatus Status { get; set; }
+        public Route Route { get; init; }
+        public PassengerModel Passenger { get; init; }
+        public DriverModel Driver { get; set; }
+        public DateTime CreateDateTime { get; init; }
+        public DateTime UpdateDateTime { get; set; }
     }
 }
