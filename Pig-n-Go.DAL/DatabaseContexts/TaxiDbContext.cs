02dd76b1672e6d3b6e1e6b99311740b223ab394c
Patisson4 <mdpoluhin@gmail.com>
2022-01-07 06:07:09 +0300

    feat: sealed db context add: OnModelConfiguring, constructor from db context options refactor: db set names

---
+++ Pig-n-Go.DAL/DatabaseContexts/TaxiDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pig_n_Go.Driver;
+using Pig_n_Go.Order;
+using Pig_n_Go.Passenger;
+
+namespace Pig_n_Go
+{
+    public sealed class TaxiDbContext : DbContext
+    {
+        public TaxiDbContext(DbContextOptions<TaxiDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<DriverModel> Drivers { get; private set; }
+        public DbSet<PassengerModel> Passengers { get; private set; }
+        public DbSet<OrderModel> Orders { get; private set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
