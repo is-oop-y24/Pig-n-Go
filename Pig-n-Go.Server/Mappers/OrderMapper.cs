492b2635f71b23519a8f4a2ee30bf91a0a124417
Arctik <edgar.saratovcev@gmail.con>
2022-01-08 12:41:17 +0300

    feat: add Order automapper

---
+++ Pig-n-Go.Server/Mappers/OrderMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Pig_n_Go.Core.DTO;
+using Pig_n_Go.Core.DTO.Order;
+using Pig_n_Go.Core.Order;
+
+namespace Pig_n_Go.Mappers.Order
+{
+    public class OrderMapper : Profile
+    {
+        public OrderMapper()
+        {
+            CreateMap<OrderCreateArguments, OrderModel>();
+            CreateMap<OrderDTO, OrderModel>();
+            CreateMap<OrderModel, OrderDTO>();
+        }
+    }
+}
