71456430fde8a9485ea5dd6486e1b116fb07b361
Arctik <edgar.saratovcev@gmail.con>
2022-01-08 11:53:11 +0300

    feat: unit tests

---
+++ Pig-n-Go.Tests/ControllerTests/DriverControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Pig_n_Go.Tests.ControllerTests
+{
+    public class DriverControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Pig-n-Go.Tests/ControllerTests/TaxiControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Pig_n_Go.Tests.ControllerTests
+{
+    public class TaxiControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
