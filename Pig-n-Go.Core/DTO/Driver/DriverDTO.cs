ef9c3654cec9d267bc2ca40419ec16dfeb57f655
Arctik <edgar.saratovcev@gmail.con>
2022-01-08 12:48:34 +0300

    feat: add driver DTO's

---
+++ Pig-n-Go.Core/DTO/Driver/DriverDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Pig_n_Go.Core.Driver;
+using Pig_n_Go.Core.Tariffs;
+using Pig_n_Go.Driver;
+
+namespace Pig_n_Go.Core.DTO.Driver
+{
+    public class DriverDTO
+    {
+        public Guid Id { get; init; }
+        public DriverInfo DriverInfo { get; set; }
+        public CarInfo CarInfo { get; set; }
+        public CartesianLocationUnit Location { get; set; }
+        public DriverRating DriverRating { get; set; }
+        public Tariff Tariff { get; set; }
+    }
+}
