89c0e4d5abb4e06e9395ed4578224db496be949a
Alexander Blazhkov <thermo911@yandex.ru>
2022-01-08 17:01:20 +0300

    fix: remove order status history logic from models

--- Pig-n-Go.Core/Order/OrderStatusHistory.cs
+++ Pig-n-Go.Core/Order/OrderStatusHistory.cs
@@ -6,6 +6,6 @@ namespace Pig_n_Go.Core.Order
     public class OrderStatusHistory
     {
         public Guid Id { get; init; } = Guid.NewGuid();
-        public SortedSet<OrderStatusHistoryEntry> Entries { get; init; } = new SortedSet<OrderStatusHistoryEntry>();
+        public List<OrderStatusHistoryEntry> Entries { get; init; } = new List<OrderStatusHistoryEntry>();
     }
 }
