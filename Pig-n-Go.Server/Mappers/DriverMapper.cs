379b0e5bdd48448a36b29f697d609af297fe6b4d
Arctik <edgar.saratovcev@gmail.con>
2022-01-08 12:48:50 +0300

    feat: add Driver automapper

---
+++ Pig-n-Go.Server/Mappers/DriverMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Pig_n_Go.Core.Driver;
+using Pig_n_Go.Core.DTO.Driver;
+
+namespace Pig_n_Go.Mappers.Order
+{
+    public class DriverMapper : Profile
+    {
+        public DriverMapper()
+        {
+            CreateMap<DriverCreateArguments, DriverModel>();
+            CreateMap<DriverDTO, DriverModel>();
+            CreateMap<DriverModel, DriverDTO>();
+        }
+    }
+}
