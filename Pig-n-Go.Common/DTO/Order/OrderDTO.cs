b4e6e4eff0b4f3ea2f1455226014fd2684e7a09e
Arctik <edgar.saratovcev@gmail.con>
2022-01-08 17:32:56 +0300

    refactor: change order DTO's

--- Pig-n-Go.Common/DTO/Order/OrderDTO.cs
+++ Pig-n-Go.Common/DTO/Order/OrderDTO.cs
@@ -9,7 +9,6 @@ namespace Pig_n_Go.Common.DTO.Order
     public class OrderDTO
     {
         public Guid Id { get; init; }
-        public OrderStatus Status { get; set; }
         public Route Route { get; init; }
         public PassengerModel Passenger { get; init; }
         public DriverModel Driver { get; set; }
@@ -17,5 +16,7 @@ namespace Pig_n_Go.Common.DTO.Order
         public DateTime UpdateDate { get; set; }
         public OrderRating Rating { get; set; }
         public Tariff Tariff { get; init; }
+        public OrderStatus Status { get; set; }
+        public OrderStatusHistory StatusHistory { get; init; } = new OrderStatusHistory();
     }
 }
