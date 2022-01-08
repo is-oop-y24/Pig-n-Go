181816d583f4f455e5005a63c41f0e077b3c6bde
Arctik <edgar.saratovcev@gmail.con>
2022-01-08 12:40:51 +0300

    feat: add order DTO's

---
+++ Pig-n-Go.Core/DTO/Order/OrderCreateArguments.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Pig_n_Go.Core.Driver;
+using Pig_n_Go.Core.Order;
+using Pig_n_Go.Core.Passenger;
+using Pig_n_Go.Core.Tariffs;
+
+namespace Pig_n_Go.Core.DTO.Order
+{
+    public class OrderCreateArguments
+    {
+        public OrderStatus Status { get; set; }
+        public Route Route { get; init; }
+        public PassengerModel Passenger { get; init; }
+        public DriverModel Driver { get; set; }
+        public DateTime CreationDate { get; init; }
+        public DateTime UpdateDate { get; set; }
+        public OrderRating Rating { get; set; }
+        public Tariff Tariff { get; set; }
+    }
+}
---
+++ Pig-n-Go.Core/DTO/Order/OrderDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Pig_n_Go.Core.Driver;
+using Pig_n_Go.Core.Order;
+using Pig_n_Go.Core.Passenger;
+using Pig_n_Go.Core.Tariffs;
+
+namespace Pig_n_Go.Core.DTO.Order
+{
+    public class OrderDTO
+    {
+        public Guid Id { get; init; }
+        public OrderStatus Status { get; set; }
+        public Route Route { get; init; }
+        public PassengerModel Passenger { get; init; }
+        public DriverModel Driver { get; set; }
+        public DateTime CreationDate { get; init; }
+        public DateTime UpdateDate { get; set; }
+        public OrderRating Rating { get; set; }
+        public Tariff Tariff { get; init; }
+    }
+}
