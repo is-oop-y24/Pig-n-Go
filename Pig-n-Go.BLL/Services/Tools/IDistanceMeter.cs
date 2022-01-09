7b347eadd50c93cb1ffab5d6980a4080a787e829
Mikhail <misha.rukovishnikow@yandex.ru>
2022-01-10 00:06:45 +0300

    feat: created DistanceMeter

---
+++ Pig-n-Go.BLL/Services/Tools/IDistanceMeter.cs
@@ -0,0 +1,9 @@
+﻿using Pig_n_Go.Core.Driver;
+
+namespace Pig_n_Go.BLL.Services.Tools
+{
+    public interface IDistanceMeter
+    {
+        decimal GetDistance(CartesianLocationUnit unit1, CartesianLocationUnit unit2);
+    }
+}
