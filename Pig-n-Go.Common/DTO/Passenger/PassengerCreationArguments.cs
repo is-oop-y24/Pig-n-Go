9432179beb7b765c7479a376bf102ee355051093
Alexander Blazhkov <thermo911@yandex.ru>
2022-01-09 09:45:13 +0300

    feat: add and configure mapper for passenger

---
+++ Pig-n-Go.Common/DTO/Passenger/PassengerCreationArguments.cs
@@ -0,0 +1,9 @@
+﻿using Pig_n_Go.Core.Passenger;
+
+namespace Pig_n_Go.Common.DTO.Passenger
+{
+    public class PassengerCreationArguments
+    {
+        public PassengerInfo PassengerInfo { get; set; }
+    }
+}
---
+++ Pig-n-Go.Server/Mappers/PassengerMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Pig_n_Go.Common.DTO.Passenger;
+using Pig_n_Go.Core.Passenger;
+
+namespace Pig_n_Go.Mappers.Order
+{
+    public class PassengerMapper : Profile
+    {
+        public PassengerMapper()
+        {
+            CreateMap<PassengerCreationArguments, PassengerModel>();
+            CreateMap<PassengerDTO, PassengerModel>();
+            CreateMap<PassengerModel, PassengerDTO>();
+        }
+    }
+}
