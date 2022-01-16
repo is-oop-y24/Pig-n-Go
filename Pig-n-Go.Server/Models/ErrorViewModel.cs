a669b5b49d5276829e368427258d6d63fbc27dff
Patisson4 <mdpoluhin@gmail.com>
2022-01-16 22:29:57 +0300

    feat: delete due to uselessness

--- Pig-n-Go.Server/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Pig_n_Go.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
