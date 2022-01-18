f3668732da4bfb9454e89b495e3d6b5b1f4d1871
Patisson4 <mdpoluhin@gmail.com>
2022-01-18 18:50:53 +0300

    refactor: remove unused Id property

--- Pig-n-Go.Core/Driver/CartesianLocationUnit.cs
+++ Pig-n-Go.Core/Driver/CartesianLocationUnit.cs
@@ -1,10 +1,7 @@
-﻿using System;
-
-namespace Pig_n_Go.Core.Driver
+﻿namespace Pig_n_Go.Core.Driver
 {
     public class CartesianLocationUnit
     {
-        public Guid Id { get; init; }
         public decimal Abscissa { get; init; }
         public decimal Ordinate { get; init; }
     }
