628151da07527108e64820080a97edb3133309f4
Patisson4 <mdpoluhin@gmail.com>
2022-01-18 03:55:17 +0300

    feat: delete redundant properties

--- Pig-n-Go.Common/DTO/Order/OrderCreationArguments.cs
+++ Pig-n-Go.Common/DTO/Order/OrderCreationArguments.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using Pig_n_Go.Core.Driver;
-using Pig_n_Go.Core.Order;
-using Pig_n_Go.Core.Passenger;
+﻿using Pig_n_Go.Core.Order;
 using Pig_n_Go.Core.Tariffs;
 
 namespace Pig_n_Go.Common.DTO.Order
@@ -9,12 +6,6 @@ namespace Pig_n_Go.Common.DTO.Order
     public class OrderCreationArguments
     {
         public Route Route { get; init; }
-        public PassengerModel Passenger { get; init; }
-        public DriverModel Driver { get; set; }
-        public DateTime CreationDate { get; init; }
-        public DateTime UpdateDate { get; set; }
-        public OrderRating Rating { get; set; }
         public Tariff Tariff { get; init; }
-        public OrderStatus Status { get; set; }
     }
 }
