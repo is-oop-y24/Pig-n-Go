ebb57848b16193fefd5e86de2b03bdecf911b860
Patisson4 <mdpoluhin@gmail.com>
2022-01-22 14:30:39 +0300

    feat: delete unused abstractions, create new tariff

---
+++ Pig-n-Go.BLL/Tariffs/TariffModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Pig_n_Go.Core.Tariffs
+{
+    public class TariffModel
+    {
+        public Guid Id { get; private init; }
+        public string Name { get; set; }
+        public decimal ChargePerLocationUnit { get; init; }
+    }
+}
