d26c1893cafa2ddff4cc8624e797e4177a9f2fbf
Patisson4 <mdpoluhin@gmail.com>
2022-01-22 14:48:11 +0300

    feat: initial commit

---
+++ Pig-n-Go.BLL/Tools/ThrowIfNullExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pig_n_Go.Core.Tools
+{
+    public static class ThrowIfNullExtension
+    {
+        public static T ThrowIfNull<T>(this T obj)
+        {
+            if (obj is null)
+                throw new ArgumentNullException(nameof(obj));
+
+            return obj;
+        }
+    }
+}
