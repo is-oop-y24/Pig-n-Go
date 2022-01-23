15e0c5d4cb221a86263f9f33398f757990a0a250
Patisson4 <81909511+Patisson4@users.noreply.github.com>
2022-01-23 23:23:27 +0300

    feat: make DriverModel own type CarInfo

--- Pig-n-Go.BLL/Driver/CarInfo.cs
+++ Pig-n-Go.BLL/Driver/CarInfo.cs
@@ -1,10 +1,7 @@
-﻿using System;
-
-namespace Pig_n_Go.Core.Driver
+﻿namespace Pig_n_Go.Core.Driver
 {
     public class CarInfo
     {
-        public Guid Id { get; init; }
         public string ModelName { get; init; }
         public string CarNumber { get; init; }
         public string Color { get; init; }
