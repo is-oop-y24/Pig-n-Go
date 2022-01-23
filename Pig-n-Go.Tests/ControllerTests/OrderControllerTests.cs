44085ddd6641c7fa620b0fa3c6c83463c1b00dc3
Patisson4 <81909511+Patisson4@users.noreply.github.com>
2022-01-23 19:09:13 +0300

    chore: remove unused test preparations

--- Pig-n-Go.Tests/ControllerTests/OrderControllerTests.cs
+++ Pig-n-Go.Tests/ControllerTests/OrderControllerTests.cs
@@ -1,51 +1,18 @@
-﻿using AutoMapper;
-using Microsoft.EntityFrameworkCore;
-using NUnit.Framework;
-using Pig_n_Go.Application.Services;
-using Pig_n_Go.Core.Services;
-using Pig_n_Go.Core.Tools;
-using Pig_n_Go.DAL.DatabaseContexts;
-using Pig_n_Go.Server.Controllers;
-using Pig_n_Go.Server.Mappers;
+﻿using NUnit.Framework;
 
 namespace Pig_n_Go.Tests.ControllerTests
 {
     public class OrderControllerTests
     {
         [SetUp]
-        public void Setup() { }
+        public void Setup()
+        {
+        }
 
         [Test]
         public void Test1()
         {
-            var optionsBuilder = new DbContextOptionsBuilder<TaxiDbContext>();
-            DbContextOptions<TaxiDbContext> options = optionsBuilder.UseSqlite($"Filename=Taxi.Database").Options;
-            var taxiDbContext = new TaxiDbContext(options);
-
-            var distanceCalculator = new NativeDistanceCalculator();
-            var maxDriverDistance = new DriverDistanceLimit { MaxValue = 10.0m };
-
-            var mapperConfig = new MapperConfiguration(
-                mc =>
-                {
-                    mc.AddProfile(new OrderMapper());
-                    mc.AddProfile(new DriverMapper());
-                });
-
-            IMapper mapper = mapperConfig.CreateMapper();
-
-            var orderService = new OrderApplication(
-                taxiDbContext,
-                mapper,
-                new OrderService(
-                    distanceCalculator,
-                    maxDriverDistance));
-
-            var passengerService = new PassengerService();
-
-            var passengerApplication = new PassengerApplication(taxiDbContext, mapper, passengerService);
-
-            var orderController = new OrderController(orderService, passengerApplication, mapper);
+            Assert.Pass();
         }
     }
 }
