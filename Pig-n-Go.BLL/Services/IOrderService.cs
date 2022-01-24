546a8a36a9647e252d6c332f2fb510146375db45
Patisson4 <81909511+Patisson4@users.noreply.github.com>
2022-01-24 03:11:00 +0300

    chore: remove unused methods

--- Pig-n-Go.BLL/Services/IOrderService.cs
+++ Pig-n-Go.BLL/Services/IOrderService.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Pig_n_Go.BLL.Driver;
 using Pig_n_Go.BLL.Order;
 
@@ -7,11 +6,8 @@ namespace Pig_n_Go.BLL.Services
 {
     public interface IOrderService
     {
-        Task<OrderModel> HandleOrderAsync(OrderModel order, List<DriverModel> drivers);
-        Task<OrderModel> CancelOrderAsync(OrderModel order);
         Task<OrderModel> AcceptOrderAsync(OrderModel order, DriverModel driver);
         Task<OrderModel> DeclineOrderAsync(OrderModel order, DriverModel driver);
-        Task<List<DriverModel>> FindClosestDrivers(List<DriverModel> drivers, CartesianLocationUnit locationUnit);
         Task<OrderModel> FinishOrderAsync(OrderModel order);
     }
 }
