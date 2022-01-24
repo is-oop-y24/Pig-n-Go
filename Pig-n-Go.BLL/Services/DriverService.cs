029348696dce20b0a0af615ff45ef2f5018c481d
Patisson4 <81909511+Patisson4@users.noreply.github.com>
2022-01-24 03:10:03 +0300

    feat: add methods GoOnline and GoOffline to service feat: updates are now run async

--- Pig-n-Go.BLL/Services/DriverService.cs
+++ Pig-n-Go.BLL/Services/DriverService.cs
@@ -1,20 +1,36 @@
-﻿using Pig_n_Go.BLL.Driver;
+﻿using System.Threading.Tasks;
+using Pig_n_Go.BLL.Driver;
 using Pig_n_Go.BLL.Order;
+using Pig_n_Go.BLL.Tariffs;
 
 namespace Pig_n_Go.BLL.Services
 {
     public class DriverService : IDriverService
     {
-        public DriverModel UpdateLocation(DriverModel driver, CartesianLocationUnit locationUnit)
+        public Task<DriverModel> UpdateLocation(DriverModel driverModel, CartesianLocationUnit locationUnit)
         {
-            driver.Location = locationUnit;
-            return driver;
+            driverModel.Location = locationUnit;
+            return Task.FromResult(driverModel);
         }
 
-        public DriverModel UpdateRating(DriverModel driverModel, OrderModel orderModel)
+        public Task<DriverModel> UpdateRating(DriverModel driverModel, OrderModel orderModel)
         {
             driverModel.DriverRating.RatingHistory.Add(orderModel.Rating);
-            return driverModel;
+            return Task.FromResult(driverModel);
+        }
+
+        public Task<DriverModel> GoOnline(DriverModel driverModel, TariffModel tariffModel)
+        {
+            driverModel.IsOnline = true;
+            driverModel.Tariff = tariffModel;
+            return Task.FromResult(driverModel);
+        }
+
+        public Task<DriverModel> GoOffline(DriverModel driverModel)
+        {
+            driverModel.IsOnline = false;
+            driverModel.Tariff = null;
+            return Task.FromResult(driverModel);
         }
     }
 }
